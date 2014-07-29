7e4498e0348b9d596a30cc288724b0bf1d420615
amfipter <amfipter@gmail.com>
2014-07-29 19:19:44 +0700

    small fix && data include

---
+++ main.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby2.0
+
+require './proc_work.rb'
+require './proc.rb'
+require 'narray'
+require 'pp'
+
+
+$data_name = ARGV[0]
+$noise_name = ARGV[1]
+$noise_cycle = 10
+
+
+Proc_main.main
--- proc.rb
+++ proc.rb
@@ -1,44 +1,39 @@
-require './proc_work.rb'
-require 'narray'
-require 'pp'
+module Proc_main
+  def main.self()
+    include Proc_work
 
-include Proc_work
+    data = read($data_name, 1)
+    noise = read($noise_name, 1)
 
-t = 10
+    noise_n = narray_create_float(noise)
+    data_n = narray_create_float(data)
 
-data_name = ARGV[0]
-noise_name = ARGV[1]
-
-data = read(data_name, 1)
-noise = read(noise_name, 1)
-
-noise_n = narray_create_float(noise)
-data_n = narray_create_float(data)
-
-noise_mean = noise_n.mean 
-data_n -= noise_mean
-noise_n -= noise_mean
-noise_mean_abs = noise_n.clone.abs.mean
-# pp noise_mean_abs
-# exit
+    noise_mean = noise_n.mean
+    data_n -= noise_mean
+    noise_n -= noise_mean
+    noise_mean_abs = noise_n.clone.abs.mean
+    # pp noise_mean_abs
+    # exit
 
 
 
 
 
-t.times do 
-  data_n = simple_noise_reduct(data_n, noise_mean_abs)
-  noise_n = simple_noise_reduct(noise_n, noise_mean_abs)
-  noise_mean_abs = noise_n.clone.abs.mean
+    $noise_cycle.times do
+      data_n = simple_noise_reduct(data_n, noise_mean_abs)
+      noise_n = simple_noise_reduct(noise_n, noise_mean_abs)
+      noise_mean_abs = noise_n.clone.abs.mean
+    end
+
+    noise_proc = narray_to_array(noise_n)
+
+
+    data_proc = narray_to_array(data_n)
+
+
+
+    puts data_proc
+
+
+  end
 end
-
-noise_proc = narray_to_array(noise_n)
-
-
-data_proc = narray_to_array(data_n)
-
-
-
-puts data_proc
-
-
