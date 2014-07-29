703718fe8f24c5ab3280996f2777dcbb09d4ad16
amfipter <amfipter@gmail.com>
2014-07-29 18:51:12 +0700

    bug :(

--- proc_work.rb
+++ proc_work.rb
@@ -1,44 +1,41 @@
-require './proc_work.rb'
-require 'narray'
-require 'pp'
+module Proc_work
+  def read(name, col = 0)
+    file = File.open(name, 'r')
+    data = Array.new
+    slice = nil
+    while(slice = file.gets)
+      data.push (slice.chomp.split ' ')[col].to_f
+    end
+    file.close
+    data
+  end
 
-include Proc_work
+  def narray_create_float(array_data)
+    narray = NArray.float(array_data.size)
+    array_data.size.times do |i|
+      narray[i] = array_data[i]
+    end
+    narray
+  end
 
-t = 10
+  def narray_to_array(narray)
+    out = Array.new 
+    narray.each do |el|
+      out.push el 
+    end
+    out
+  end
 
-data_name = ARGV[0]
-noise_name = ARGV[1]
+  def simple_noise_reduct(data, mean_abs)
+    data.length.times do |i|
+      data[i] += mean_abs if data[i] < 0
+      data[i] -= mean_abs if data[i] > 0
+    end
+    data 
+  end
 
-data = read(data_name, 1)
-noise = read(noise_name, 1)
+  def find_noise(ndata)
+    nil
+  end
 
-noise_n = narray_create_float(noise)
-data_n = narray_create_float(data)
-
-noise_mean = noise_n.mean 
-data_n -= noise_mean
-noise_n -= noise_mean
-noise_mean_abs = noise_n.clone.abs.mean
-# pp noise_mean_abs
-# exit
-
-
-
-
-
-t.times do 
-  data_n = simple_noise_reduct(data_n, noise_mean_abs)
-  noise_n = simple_noise_reduct(noise_n, noise_mean_abs)
-  noise_mean_abs = noise_n.clone.abs.mean
 end
-
-noise_proc = narray_to_array(noise_n)
-
-
-data_proc = narray_to_array(data_n)
-
-
-
-puts data_proc
-
-
