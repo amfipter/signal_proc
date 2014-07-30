4abc0547c6332725c808628a33f94f61176409ef
amfipter <amfipter@gmail.com>
2014-07-31 02:20:49 +0700

    refactor proc.rb && create new file for additional tools

--- proc.rb
+++ proc.rb
@@ -1,13 +1,12 @@
 module Proc_main
   include Proc_work
-  def self.main()
-    
+  def self.main()   
 
-    data = Proc_work.read($data_name, 1)
-    noise = Proc_work.read($noise_name, 1)
+    data = read($data_name, 1)
+    noise = read($noise_name, 1)
 
-    noise_n = Proc_work.narray_create_float(noise)
-    data_n = Proc_work.narray_create_float(data)
+    noise_n = narray_create_float(noise)
+    data_n = narray_create_float(data)
 
     noise_mean = noise_n.mean
     data_n -= noise_mean
@@ -16,25 +15,62 @@ module Proc_main
     # pp noise_mean_abs
     # exit
 
-
-
-
-
     $noise_cycle.times do
-      data_n = Proc_work.simple_noise_reduct(data_n, noise_mean_abs)
-      noise_n = Proc_work.simple_noise_reduct(noise_n, noise_mean_abs)
+      data_n = simple_noise_reduct(data_n, noise_mean_abs)
+      noise_n = simple_noise_reduct(noise_n, noise_mean_abs)
       noise_mean_abs = noise_n.clone.abs.mean
     end
 
-    noise_proc = Proc_work.narray_to_array(noise_n)
+    noise_proc = narray_to_array(noise_n)
 
 
-    data_proc = Proc_work.narray_to_array(data_n)
+    data_proc = narray_to_array(data_n)
 
 
 
     puts data_proc
 
 
+  end
+
+  private
+
+  def self.read(name, col = 0)
+    file = File.open(name, 'r')
+    data = Array.new
+    slice = nil
+    while(slice = file.gets)
+      data.push (slice.chomp.split ' ')[col].to_f
+    end
+    file.close
+    data
+  end
+
+  def self.narray_create_float(array_data)
+    narray = NArray.float(array_data.size)
+    array_data.size.times do |i|
+      narray[i] = array_data[i]
+    end
+    narray
+  end
+
+  def self.narray_to_array(narray)
+    out = Array.new 
+    narray.each do |el|
+      out.push el 
+    end
+    out
+  end
+
+  def self.simple_noise_reduct(data, mean_abs)
+    data.length.times do |i|
+      data[i] += mean_abs if data[i] < 0
+      data[i] -= mean_abs if data[i] > 0
+    end
+    data 
+  end
+
+  def self.find_noise(ndata)
+    nil
   end
 end
