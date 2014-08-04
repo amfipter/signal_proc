c21b68453c88be36a49eb9fadd5861a0f2d52506
amfipter <amfipter@gmail.com>
2014-08-04 21:18:22 +0700

    timemarks parse module

--- tools.rb
+++ tools.rb
@@ -15,3 +15,19 @@ module Tools
     nil 
   end
 end
+
+module Timemarks_parse
+  def self.get_membership_function(file_name)
+    mf_data = Hash.new
+    f = File.open(file_name, 'r')
+    slice = nil
+    while(slice = f.getc)
+      data = slice.chomp.split(' ')
+      (data[0].to_f..data[1].to_f).step(1.0/300.0) do |i|
+        mf_data[i] = data[3].to_i
+      end
+    end
+    f.close
+    mf_data
+  end
+end
